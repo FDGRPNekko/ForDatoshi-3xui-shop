de669c85c2e78593737fe56b58cd4359fbf74de6
Анонимный разработчик (JAVA - C++) <147281484+FDGRPNekko@users.noreply.github.com>
2026-01-14 00:28:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ read_input_yn() {
 }
 
 REPO_URL="https://github.com/FDGRPNekko/ForDatoshi-3xui-shop.git"
-PROJECT_DIR="3xui-shopbot"
+PROJECT_DIR="ForDatoshi-3xui-shop"
 NGINX_CONF_FILE="/etc/nginx/sites-available/${PROJECT_DIR}.conf"
 
 echo -e "${GREEN}--- Запуск скрипта установки/обновления 3xui-ShopBot ---${NC}"
